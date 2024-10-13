c19bf9489d3a85c43468343bcdf3fe120fc1aa10
Ale-uwu <c8534312@gmail.com>
2024-10-13 09:58:36 +0200

    coso de java

---
+++ javaVS/helloWorld.java
@@ -0,0 +1,12 @@
+public class helloWorld{
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println(suma(4,5));
+    }
+
+    public static int suma(int n1,int n2){
+        return n1+n2;
+    }
+
+}
